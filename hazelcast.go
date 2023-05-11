ae32951e7ed73d20d437b00da9dc400107b4d60c
Lina <linasmith69@yahoo.com>
2023-05-11 14:51:32 +0300

    add delete and incr and decr

--- hazelcast.go
+++ hazelcast.go
@@ -4,32 +4,78 @@ import (
 	"context"
 
 	"github.com/hazelcast/hazelcast-go-client"
+	"go.k6.io/k6/js/modules"
 )
 
-type HazelCast struct{}
+func init() {
+	modules.Register("k6/x/hazelcast", new(HazelCast))
+}
 
-func (HazelCast) Connect(ctx context.Context, addrs []string) (*hazelcast.Client, error) {
+type HazelCast struct {
+	ctx context.Context
+}
+
+func (hc *HazelCast) Connect(addrs []string) (*hazelcast.Client, error) {
+
+	hc.ctx = context.TODO()
 	config := hazelcast.Config{}
 
 	if len(addrs) == 0 {
 		addrs = append(make([]string, 0), "localhost:5701")
 	}
 	config.Cluster.Network.SetAddresses(addrs...)
-	client, err := hazelcast.StartNewClientWithConfig(ctx, config)
+	client, err := hazelcast.StartNewClientWithConfig(hc.ctx, config)
+
 	return client, err
 }
 
-func (HazelCast) GetMap(ctx context.Context, client *hazelcast.Client) (*hazelcast.Map, error) {
-	testMap, err := client.GetMap(ctx, "test")
-	return testMap, err
+func (hc *HazelCast) GetMap(client *hazelcast.Client, name string) (*hazelcast.Map, error) {
+	mapping, err := client.GetMap(hc.ctx, name)
+	return mapping, err
 }
 
-func (HazelCast) Set(ctx context.Context, testMap *hazelcast.Map, key string, value interface{}) error {
-	err := testMap.Set(ctx, key, value)
+func (hc *HazelCast) Set(testMap *hazelcast.Map, key string, value interface{}) error {
+	err := testMap.Set(hc.ctx, key, value)
 	return err
 }
 
-func (HazelCast) Get(ctx context.Context, testMap *hazelcast.Map, key string) (interface{}, error) {
-	value, err := testMap.Get(ctx, key)
+func (hc *HazelCast) Put(testMap *hazelcast.Map, key string, value interface{}) (interface{}, error) {
+	value, err := testMap.Put(hc.ctx, key, value)
 	return value, err
+}
+
+func (hc *HazelCast) Get(testMap *hazelcast.Map, key string) (interface{}, error) {
+	value, err := testMap.Get(hc.ctx, key)
+	return value, err
+}
+
+func (hc *HazelCast) Del(testMap *hazelcast.Map, key string) error {
+	err := testMap.LoadAllReplacing(hc.ctx, key)
+	return err
+}
+
+// Counter operations
+
+func (hc *HazelCast) getPNCounter(client *hazelcast.Client, name string) (*hazelcast.PNCounter, error) {
+	pNCounter, err := client.GetPNCounter(hc.ctx, name)
+	return pNCounter, err
+}
+
+func (hc *HazelCast) Incr(client *hazelcast.Client, name string) error {
+	pNCounter, err := hc.getPNCounter(client, name)
+	if err != nil {
+		return err
+	}
+	_, err = pNCounter.IncrementAndGet(hc.ctx)
+	return err
+}
+
+func (hc *HazelCast) Decr(client *hazelcast.Client, name string) error {
+	pNCounter, err := hc.getPNCounter(client, name)
+	if err != nil {
+		return err
+	}
+	_, err = pNCounter.DecrementAndGet(hc.ctx)
+	return err
+
 }
